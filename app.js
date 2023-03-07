e016f1da171af4455dae1101022ba9f56f9a3f85
KaedenOC <komeacarr2@gmail.com>
2023-03-07 13:45:13 -0800

    greeting prompt first question

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 "use strict";
 
+let userName = prompt("Hello! What is your name?");
+console.log(userName);
+alert("Welcome! " + userName + " to my site.");
+
+alert("I have a quick guessing game for you!");
+
+let favThing = prompt("My favorite thing to do is snowboard. Yes or No?");
+console.log(favThing);
+
+if (favThing === "Yes"){
+    alert("Indeed! My favorite thing to do is Snowboard.").toLowerCase;
+} else {
+    alert("I'm sorry that was not the right answer. Please try again.");
+}
+
