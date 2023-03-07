551527aeeb146492b4c23b1df81959c7839b9aac
KaedenOC <komeacarr2@gmail.com>
2023-03-07 14:27:35 -0800

    fixed js prompts

--- app.js
+++ app.js
@@ -6,12 +6,25 @@ alert("Welcome! " + userName + " to my site.");
 
 alert("I have a quick guessing game for you!");
 
-let favThing = prompt("My favorite thing to do is snowboard. Yes or No?");
+let favThing = prompt("My favorite thing to do is snowboard. Yes or No?").toLowerCase();
 console.log(favThing);
 
-if (favThing === "Yes"){
-    alert("Indeed! My favorite thing to do is Snowboard.").toLowerCase;
+if (favThing === "yes"){
+    alert("Indeed. My favorite thing to do is Snowboard.");
+    console.log(favThing);
+
+} else {
+    alert("Im sorry that was not the right answer. Please try again.");
+}
+
+alert("Next Question");
+
+let favFood = prompt("My favorite food is Pizza. Yes or No?").toLowerCase();
+
+if (favFood === "yes"){
+    alert("Correct! Pizza is my favorite food.");
+    console.log(favFood);
+
 } else {
     alert("I'm sorry that was not the right answer. Please try again.");
 }
-
