3d627d26b4c2dce955f22395e98dd6d687817a57
KaedenOC <komeacarr2@gmail.com>
2023-03-07 14:46:45 -0800

    finished js prompts

--- app.js
+++ app.js
@@ -11,20 +11,46 @@ console.log(favThing);
 
 if (favThing === "yes"){
     alert("Indeed. My favorite thing to do is Snowboard.");
-    console.log(favThing);
 
 } else {
     alert("Im sorry that was not the right answer. Please try again.");
 }
 
-alert("Next Question");
-
 let favFood = prompt("My favorite food is Pizza. Yes or No?").toLowerCase();
+console.log(favFood);
 
 if (favFood === "yes"){
     alert("Correct! Pizza is my favorite food.");
-    console.log(favFood);
 
+} else {
+    alert("I'm sorry that was not the right answer. Please try again.");
+}
+
+let myCareer = prompt("My past career job was a UPS Package Delivery Driver Yes or No?").toLowerCase();
+console.log(myCareer);
+
+if (myCareer === "yes"){
+    alert("Yes! That's right.");
+} else {
+    alert("I'm sorry that was not the right answer. Please try again.");
+}
+
+alert("We are almost done!");
+
+let favAnimal = prompt("My favorite animals are Alaskan Malamutes. Yes or no?").toLowerCase();
+console.log(favAnimal);
+
+if (favAnimal === "yes"){
+    alert("Perfect!");
+} else {
+    alert("I'm sorry that was not the right answer. Please try again.");
+}
+
+let myGoal = prompt("My overall goal is to become a Developer. Yes or No?").toLowerCase();
+console.log(myGoal);
+
+if (myGoal === "yes"){
+    alert("Thats it! Thank you " + userName + " for taking the time to answer these questions.")
 } else {
     alert("I'm sorry that was not the right answer. Please try again.");
 }
