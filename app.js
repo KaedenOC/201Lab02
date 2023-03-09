5460b01da07e45161d926e208da6a1aeef270bdd
KaedenOC <komeacarr2@gmail.com>
2023-03-09 14:00:31 -0800

    js changes with functions

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 "use strict";
 
+function greeting(){
+
 let userName = prompt("Hello! What is your name?");
-// console.log(userName);
-alert("Welcome! " + userName + " to my site.");
-
-alert("I have a quick guessing game for you!");
-
-let favThing = prompt("My favorite thing to do is snowboard. Yes or No?").toLowerCase();
-// console.log(favThing);
-
-if (favThing === "yes"){
-    alert("Indeed. My favorite thing to do is Snowboard.");
-
-} else {
-    alert("Im sorry that was not the right answer. Please try again.");
+    console.log(userName);
+    alert("Welcome! " + userName + " to my site.");
 }
+greeting()
+
+    alert("I have a quick guessing game for you!");
+
+function guessingGame1(){
+    let favThing = prompt("My favorite thing to do is snowboard. Yes or No?").toLowerCase();
+    console.log(favThing);
+    if (favThing === "yes"){
+        alert("Indeed. My favorite thing to do is Snowboard.");
+    } else {
+        alert("Im sorry that was not the right answer. Please try again.");
+    }
+}
+guessingGame1()
+
 
 let favFood = prompt("My favorite food is Pizza. Yes or No?").toLowerCase();
 // console.log(favFood);
