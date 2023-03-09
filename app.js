ae788516ee320c20c5ade4cb983c669eec8d1dd2
KaedenOC <komeacarr2@gmail.com>
2023-03-08 16:42:23 -0800

    uncommented previous lab js

--- app.js
+++ app.js
@@ -1,59 +1,59 @@
 "use strict";
 
-// let userName = prompt("Hello! What is your name?");
-// // console.log(userName);
-// alert("Welcome! " + userName + " to my site.");
+let userName = prompt("Hello! What is your name?");
+// console.log(userName);
+alert("Welcome! " + userName + " to my site.");
 
-// alert("I have a quick guessing game for you!");
+alert("I have a quick guessing game for you!");
 
-// let favThing = prompt("My favorite thing to do is snowboard. Yes or No?").toLowerCase();
-// // console.log(favThing);
+let favThing = prompt("My favorite thing to do is snowboard. Yes or No?").toLowerCase();
+// console.log(favThing);
 
-// if (favThing === "yes"){
-//     alert("Indeed. My favorite thing to do is Snowboard.");
+if (favThing === "yes"){
+    alert("Indeed. My favorite thing to do is Snowboard.");
 
-// } else {
-//     alert("Im sorry that was not the right answer. Please try again.");
-// }
+} else {
+    alert("Im sorry that was not the right answer. Please try again.");
+}
 
-// let favFood = prompt("My favorite food is Pizza. Yes or No?").toLowerCase();
-// // console.log(favFood);
+let favFood = prompt("My favorite food is Pizza. Yes or No?").toLowerCase();
+// console.log(favFood);
 
-// if (favFood === "yes"){
-//     alert("Correct! Pizza is my favorite food.");
+if (favFood === "yes"){
+    alert("Correct! Pizza is my favorite food.");
 
-// } else {
-//     alert("I'm sorry that was not the right answer. Please try again.");
-// }
+} else {
+    alert("I'm sorry that was not the right answer. Please try again.");
+}
 
-// let myCareer = prompt("My past career job was a UPS Package Delivery Driver Yes or No?").toLowerCase();
-// // console.log(myCareer);
+let myCareer = prompt("My past career job was a UPS Package Delivery Driver Yes or No?").toLowerCase();
+// console.log(myCareer);
 
-// if (myCareer === "yes"){
-//     alert("Yes! That's right.");
-// } else {
-//     alert("I'm sorry that was not the right answer. Please try again.");
-// }
+if (myCareer === "yes"){
+    alert("Yes! That's right.");
+} else {
+    alert("I'm sorry that was not the right answer. Please try again.");
+}
 
-// alert("We are almost done!");
+alert("We are almost done!");
 
-// let favAnimal = prompt("My favorite animals are Alaskan Malamutes. Yes or no?").toLowerCase();
-// // console.log(favAnimal);
+let favAnimal = prompt("My favorite animals are Alaskan Malamutes. Yes or no?").toLowerCase();
+// console.log(favAnimal);
 
-// if (favAnimal === "yes"){
-//     alert("Perfect!");
-// } else {
-//     alert("I'm sorry that was not the right answer. Please try again.");
-// }
+if (favAnimal === "yes"){
+    alert("Perfect!");
+} else {
+    alert("I'm sorry that was not the right answer. Please try again.");
+}
 
-// let myGoal = prompt("My overall goal is to become a Developer. Yes or No?").toLowerCase();
-// // console.log(myGoal);
+let myGoal = prompt("My overall goal is to become a Developer. Yes or No?").toLowerCase();
+// console.log(myGoal);
 
-// if (myGoal === "yes"){
-//     alert("Thats it! Thank you " + userName + " for taking the time to answer these questions.")
-// } else {
-//     alert("I'm sorry that was not the right answer. Please try again.");
-// }
+if (myGoal === "yes"){
+    alert("Thats it! Thank you " + userName + " for taking the time to answer these questions.")
+} else {
+    alert("I'm sorry that was not the right answer. Please try again.");
+}
 
 let questions = ["How old am I? Pick a number between 1 and 30", "I am thinking of 3 numbers between 1 and 10. Can you guess what they are?"];
 let answers = [25, 2, 5, 9];
